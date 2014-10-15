9707eb829eb307f04c7ee8b217f9e10593cda74f
Nick Ward <ward.nickjames@gmail.com>
2014-10-15 11:42:17 +0100

    Filter for numeric columns

--- src/js/app.js
+++ src/js/app.js
@@ -1,115 +1,124 @@
 /** @jsx React.DOM */
+// vim: sw=2:sts=2
 
 var React = require('react');
 var $ = require('jquery');
 var cities = require('./cities');
-
-var App = React.createClass({
-    getInitialState: function() {
-        return {filterText: ''};
-    },
-    filterCallback: function(filterText) {
-        this.setState({filterText: filterText});
-    },
-    render: function() {
-        return (
-            <div>
-                <SearchBar
-                    filterText={this.state.filterText}
-                    filterCallback={this.filterCallback}
-                />
-                <Table 
-                    schema={this.props.schema}
-                    data={this.props.data}
-                    filterText={this.state.filterText}
-                />
-            </div>
-        );
-    }
-});
+var R = require('ramda');
 
 var Table = React.createClass({
-    render: function() {
-        var rows = this.props.data.map(function(row) {
-            return (
-                <DataRow
-                    schema={this.props.schema}
-                    data={row}
-                    filterText={this.props.filterText}
-                />
-            );
-        }, this);
-        return (
-            <table>
-                <thead><HeaderRow schema={this.props.schema}/></thead>
-                <tbody>{rows}</tbody>
-            </table>
-        );
-    }
-});
-
-var DataRow = React.createClass({
-    render: function() {
-        var row = this.props.schema.map(function(column) {
-            var val = this.props.data[column.id];
-            var empty = val===undefined || val===null;
-            return empty ?
-                <EmptyCell/> : 
-                <Cell
-                    value={this.props.data[column.id]}
-                    filterText={this.props.filterText}
-                />
-        }, this);
-        return <tr>{row}</tr>;
-    }
-});
-
-var Cell = React.createClass({
-    render: function() {
-        var val = this.props.value.toString()
-        var filterText = this.props.filterText.toLowerCase();
-        if (filterText && val.toLowerCase().indexOf(filterText) >= 0) {
-            return <td className="selected">{val}</td>
-        }
-        return <td>{val}</td>
-    }
-});
-
-var EmptyCell = React.createClass({
-    render: function() {
-        return <td className="empty">empty</td>
-    }
+  render: function() {
+    var rows = this.props.data.map(function(row) {
+      return <DataRow schema={this.props.schema} data={row}/>;
+    }, this);
+    return (
+      <table>
+        <thead><HeaderRow schema={this.props.schema}/></thead>
+        <tbody>{rows}</tbody>
+      </table>
+    );
+  }
 });
 
 var HeaderRow = React.createClass({
-    render: function() {
-        var row = this.props.schema.map(function(column) {
-            return <th>{column.name}</th>
-        });
-        return <tr>{row}</tr>
-    }
+  render: function() {
+    var row = this.props.schema.map(function(column) {
+      return <th>{column.name}</th>
+    });
+    return <tr>{row}</tr>;
+  }
 });
 
-var SearchBar = React.createClass({
-    onChange: function() {
-        this.props.filterCallback(this.refs.filterInput.getDOMNode().value);
-    },
-    render: function() {
-        return (
-            <input
-                type="text"
-                placeholder="filter.."
-                ref="filterInput"
-                value={this.props.filterText}
-                onChange={this.onChange}
-            />
-        )
-    }
+var DataRow = React.createClass({
+  render: function() {
+    var row = this.props.schema.map(function(column) {
+      var val = this.props.data[column.id];
+      var empty = val===undefined || val===null;
+      return empty ? <EmptyCell/> : <Cell value={this.props.data[column.id]}/>
+    }, this);
+    return <tr>{row}</tr>;
+  }
+});
+
+var Cell = React.createClass({
+  render: function() {
+    return <td>{this.props.value}</td>;
+  }
+});
+
+var EmptyCell = React.createClass({
+  render: function() {
+    return <td className="empty">empty</td>;
+  }
+});
+
+var NumericFilter = React.createClass({
+  onChange: function() {
+    this.props.filterCallback(this.getFilter());
+  },
+  getValue: function(ref) {
+    return this.refs[ref].getDOMNode().value;
+  },
+  getFilter: function() {
+    var num = this.getValue("number");
+    if (!num && num!==0) return R.identity;
+    var op = R[this.getValue("operation")];
+    var col = this.getValue("column");
+    var spec = {}
+    spec[col] = function(val) { return op(val, num); };
+    return R.filter(R.where(spec));
+  },
+  render: function() {
+    var numeric = R.filter(R.where({'type': 'number'}), this.props.schema);
+    return (
+      <form onChange={this.onChange} className="filter-widget">
+        <ColumnSelect columns={numeric} ref="column"/>
+        <select ref="operation">
+          <option value="gt">{'>'}</option>
+          <option value="lt">{'<'}</option>
+          <option value="eq">{'='}</option>
+        </select>
+        <input ref="number" type="number" placeholder="enter number..."/>
+      </form>
+    )
+  }
+});
+
+var ColumnSelect = React.createClass({
+  render: function() {
+    return (
+      <select> {this.props.columns.map(function(col, i) {
+          return <option value={col.id}>{col.name}</option>
+        }, this)}
+      </select>
+    )
+  },
+});
+
+var App = React.createClass({
+  getInitialState: function() {
+    return {filter: R.identity} 
+  },
+  filterCallback: function(filter) {
+    this.setState({filter: filter});
+  },
+  render: function() {
+    var data = this.state.filter(this.props.data);
+    return (
+      <div>
+        <NumericFilter
+          filterCallback={this.filterCallback}
+          schema={this.props.schema}
+        />
+        <Table schema={this.props.schema} data={data}/>
+      </div>
+    );
+  }
 });
 
 $(function() {
-    React.renderComponent(
-        <App data={cities.data} schema={cities.schema}/>,
-        document.getElementById("mountNode")
-    );
+  React.renderComponent(
+    <App data={cities.data} schema={cities.schema}/>,
+    document.getElementById("mountNode")
+  );
 });
--- src/js/cities.js
+++ src/js/cities.js
@@ -2,161 +2,161 @@
 exports.data = [
     {
         "city":"Shanghai",
-        "population":"24150000",
+        "population":24150000,
         "definition":"Direct-controlled municipality",
-        "area_km2":"6340.5",
-        "density_km2":"3809",
+        "area_km2":6340.5,
+        "density_km2":3809,
         "country":"China"
     },
     {
         "city":"Karachi",
-        "population":"23500000",
+        "population":23500000,
         "definition":"City District Government",
-        "area_km2":"3527",
-        "density_km2":"6663",
+        "area_km2":3527,
+        "density_km2":6663,
         "country":"Pakistan"
     },
     {
         "city":"Beijing",
-        "population":"21150000",
+        "population":21150000,
         "definition":"Direct-controlled municipality",
-        "area_km2":"16410.54",
-        "density_km2":"1289",
+        "area_km2":16410.54,
+        "density_km2":1289,
         "country":"China"
     },
     {
         "city":"Delhi",
-        "population":"17838842",
+        "population":17838842,
         "definition":"National Capital Region",
-        "area_km2":"1484.0",
-        "density_km2":"7845.9",
+        "area_km2":1484.0,
+        "density_km2":7845.9,
         "country":"India"
     },
     {
         "city":"Lagos",
-        "population":"17060307",
+        "population":17060307,
         "definition":"Metropolitan City",
-        "area_km2":"999.58",
-        "density_km2":"17068",
+        "area_km2":999.58,
+        "density_km2":17068,
         "country":"Nigeria"
     },
     {
         "city":"Istanbul",
-        "population":"14160467",
+        "population":14160467,
         "definition":"Metropolitan Municipality-Province",
-        "area_km2":"2189.79",
-        "density_km2":"6467",
+        "area_km2":2189.79,
+        "density_km2":6467,
         "country":"Turkey"
     },
     {
         "city":"Guangzhou",
-        "population":"12700800",
+        "population":12700800,
         "definition":"Sub-Provincial City",
-        "area_km2":"3843.43",
-        "density_km2":"3305",
+        "area_km2":3843.43,
+        "density_km2":3305,
         "country":"China"
     },
     {
         "city":"Mumbai",
-        "population":"12655220",
+        "population":12655220,
         "definition":"Municipal Corporation",
-        "area_km2":"603.4",
-        "density_km2":"20680",
+        "area_km2":603.4,
+        "density_km2":20680,
         "country":"India"
     },
     {
         "city":"Moscow",
-        "population":"12111194",
+        "population":12111194,
         "definition":"Federal City",
-        "area_km2":"2510.12",
-        "density_km2":"4825",
+        "area_km2":2510.12,
+        "density_km2":4825,
         "country":"Russia"
     },
     {
         "city":"Dhaka",
-        "population":"12043977",
+        "population":12043977,
         "definition":"City Corporation",
-        "area_km2":"302.92",
-        "density_km2":"39760",
+        "area_km2":302.92,
+        "density_km2":39760,
         "country":"Bangladesh"
     },
     {
         "city":"Cairo",
-        "population":"11922949",
+        "population":11922949,
         "definition":"Governorate",
-        "area_km2":"3085.1",
-        "density_km2":"3864",
+        "area_km2":3085.1,
+        "density_km2":3864,
         "country":"Egypt"
     },
     {
         "city":"São Paulo",
-        "population":"11895893",
+        "population":11895893,
         "definition":"Municipality City",
-        "area_km2":"1521.11",
-        "density_km2":"7762.3",
+        "area_km2":1521.11,
+        "density_km2":7762.3,
         "country":"Brazil"
     },
     {
         "city":"Lahore",
-        "population":"11318745",
+        "population":11318745,
         "definition":"City District",
-        "area_km2":"1772",
-        "density_km2":"3566",
+        "area_km2":1772,
+        "density_km2":3566,
         "country":"Pakistan"
     },
     {
         "city":"Shenzhen",
-        "population":"10467400",
+        "population":10467400,
         "definition":"Sub-Provincial City",
-        "area_km2":"1991.64",
-        "density_km2":"5255",
+        "area_km2":1991.64,
+        "density_km2":5255,
         "country":"China"
     },
     {
         "city":"Seoul",
-        "population":"10388055",
+        "population":10388055,
         "definition":"Special City",
-        "area_km2":"605.21",
-        "density_km2":"17164",
+        "area_km2":605.21,
+        "density_km2":17164,
         "country":"South Korea"
     },
     {
         "city":"Bangalore",
-        "population":"10178146",
-        "area_km2":"709.5",
-        "density_km2":"11876",
+        "population":10178146,
+        "area_km2":709.5,
+        "density_km2":11876,
         "country":"India"
     },
     {
         "city":"Jakarta",
-        "population":"9988329",
+        "population":9988329,
         "definition":"Capital Region - Five Kota",
-        "area_km2":"664.12",
-        "density_km2":"15040",
+        "area_km2":664.12,
+        "density_km2":15040,
         "country":"Indonesia"
     },
     {
         "city":"Kinshasa",
-        "population":"9735000",
+        "population":9735000,
         "definition":"Metropolitan Municipality-Province",
-        "area_km2":"1117.62",
-        "density_km2":"8710",
+        "area_km2":1117.62,
+        "density_km2":8710,
         "country":"Democratic Republic of the Congo"
     },
     {
         "city":"Tianjin",
-        "population":"9341844",
+        "population":9341844,
         "definition":"Direct-controlled municipality",
-        "area_km2":"4037",
-        "density_km2":"2314",
+        "area_km2":4037,
+        "density_km2":2314,
         "country":"China"
     },
     {
         "city":"Tokyo",
-        "population":"9071577",
+        "population":9071577,
         "definition":"23 Special Wards (tokubetsu-ku)",
-        "area_km2":"622.99",
-        "density_km2":"14562",
+        "area_km2":622.99,
+        "density_km2":14562,
         "country":"Japan"
     }
 ];
@@ -165,25 +165,31 @@ exports.schema = [
     {
         'id': 'city',
         'name': 'City',
+        'type': 'string'
     },
     {
         'id': 'population',
         'name': 'Population',
+        'type': 'number'
     },
     {
         'id': 'definition',
         'name': 'Definition',
+        'type': 'string'
     },
     {
         'id': 'area_km2',
         'name': 'Area (km&sup2;)',
+        'type': 'number'
     },
     {
         'id': 'density_km2',
         'name': 'Population density (km&sup2;)',
+        'type': 'number'
     },
     {
         'id': 'country',
         'name': 'Country',
+        'type': 'enum'
     }
 ]
