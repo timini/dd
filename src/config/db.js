0fa0c2d001cbd10dfdaa89c0f37d73956970a415
Bill <bill@administrators-iMac-3.local>
2014-10-24 00:20:14 +0100

    syncdb gulp task

--- src/config/db.js
+++ src/config/db.js
@@ -1,8 +1,20 @@
 models = require(BASE('/src/models'));
 
 module.exports.init = function(db_conn){
-    for (var model in model){
-        models[model](db_conn);
+    Models = {};
+    for (var model in models){
+        Model = models[model](db_conn);
+        Models[model] = Model;
     }
-    return models;
+    return Models;
+}
+
+module.exports.sync = function(models){
+    for (var model in models){
+        console.log('syncing ' + model);
+        models[model].sync(function(err){
+            if (err) { console.log('error syncing ' + model); }
+            else { console.log('syncing complete for ' + model); }
+        });
+    }
 }
