525e4c31ffd7839928db465ecfafad9e608e9273
Nick Ward <ward.nickjames@gmail.com>
2014-10-24 12:30:13 +0100

    NO GLOBALZgit log -n 22

--- src/backend/config/environment.js
+++ src/backend/config/environment.js
@@ -1,24 +1,13 @@
-var cookieParser    = require('cookie-parser');
-var bodyParser      = require('body-parser');
-var logger          = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var logger = require('morgan');
 
 module.exports = function(app){
-  app.set('views', '../views');
+  app.set('views', './src/backend/views');
   app.set('view engine', 'jade');
 
   app.use(logger('dev'));
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(cookieParser());
-
-  app.use(function(req,res,next){
-    models(function(err,db){
-      if (err) return next(err);
-
-      req.models = db.models;
-      req.db = db;
-
-      return next();
-    });
-  });
 }
--- src/backend/config/routes.js
+++ src/backend/config/routes.js
@@ -1,8 +1,11 @@
-var express     = require('express')
+var express = require('express');
+var path = require('path');
 
 var controllers = require('../controllers');
 
 var router = express.Router();
+
+var rootPath = path.dirname(require.main.filename);
 
 router.get('/', controllers.home);
 router.get('/user/login', controllers.users.login);
@@ -10,8 +13,8 @@ router.get('/user/logout', controllers.users.logout);
 router.post('/user/create', controllers.users.create);
 router.get('/product', controllers.product.list);
 router.get('/product/:product_group', controllers.product.get);
-router.use('/js', express.static(__dirname + '/build/js'));
-router.use('/css', express.static(__dirname + '/build/css'));
-router.use('/resources', express.static(__dirname + '/resources'));
+router.use('/js', express.static(rootPath + '/build/js'));
+router.use('/css', express.static(rootPath + '/build/css'));
+router.use('/resources', express.static(rootPath + '/resources'));
 
 module.exports = router;
