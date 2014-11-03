62cfa10cf3970eb87a52d0d2edcc7285eb4f3081
timrich <tim@rewire.it>
2014-11-03 04:03:28 +0000

    i hate javascript

--- backend/config/environment.js
+++ backend/config/environment.js
@@ -1,6 +1,7 @@
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var logger = require('morgan');
+var session = require('express-session');
 
 module.exports = function(app){
   app.set('views', './backend/views');
@@ -9,6 +10,9 @@ module.exports = function(app){
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(cookieParser());
+  app.use(session({secret: 'lalala', 
+                   saveUninitialized: true,
+                   resave: true}));
 
   // dev stuff
   app.use(logger('dev'));
---
+++ backend/modules/auth/routes.js
@@ -0,0 +1,18 @@
+
+
+var express = require('express');
+
+var router = express.Router();
+
+module.exports = function(passport){
+ 
+  /* GET login page. */
+  router.get('/', function(req, res) { res.send('login form'); });
+  /* Handle Login POST */
+  router.post('/login', passport.authenticate('login'));
+  /* GET Registration Page */
+  /* Handle Registration POST */
+  router.post('/create', passport.authenticate('register'));
+ 
+  return router;
+}
