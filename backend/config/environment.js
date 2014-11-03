0976d7db9a24e45ff04d55f5c5a6a48343e97fe4
tim <tim@rewire.it>
2014-11-03 07:10:04 +0200

    test not passing

--- backend/config/environment.js
+++ backend/config/environment.js
@@ -2,17 +2,20 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var logger = require('morgan');
 var session = require('express-session');
+var passport = require('passport')
 
 module.exports = function(app){
   app.set('views', './backend/views');
   app.set('view engine', 'jade');
 
+  app.use(cookieParser());
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: false }));
-  app.use(cookieParser());
   app.use(session({secret: 'lalala', 
                    saveUninitialized: true,
                    resave: true}));
+  app.use(passport.initialize());
+  app.use(passport.session());
 
   // dev stuff
   app.use(logger('dev'));
